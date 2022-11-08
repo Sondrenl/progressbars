b81835eaf4b89a6e648f33ebc658545236a568cf
Sondrenl <sondrelassen@gmail.com>
2022-11-08 10:49:38 +0100

    improved readability, minor fixes

--- main.py
+++ main.py
@@ -8,69 +8,73 @@ from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
 
 
-# Window.size = (428, 926)
+Window.size = (500, 926)
+Window.set_icon('icon.png')
+Window.set_title('Progressbars')
+# Window.grab_mouse()
 
 
 class TimerCard(MDCard):
     # set variables to be used
     event = None
-    targettime = NumericProperty(0)
-    currenttime = NumericProperty(0)
-    displaytime = NumericProperty(0)
-    selecttime = NumericProperty(0)
-    progressbaropacity = NumericProperty(0)
-    startbuttonopacity = NumericProperty(100)
+    target_time = NumericProperty(0)
+    current_time = NumericProperty(0)
+    display_time = NumericProperty(0)
+    select_time = NumericProperty(0)
+    progressbar_opacity = NumericProperty(0)
+    start_button_opacity = NumericProperty(100)
     disabled = False
 
-    # change targettime when buttons are pressed
-    def change_targettime(self, time):
-        self.targettime += time
-        self.displaytime += time
-        self.selecttime += time
+    # change target_time when buttons are pressed
+    def change_target_time(self, time):
+        self.target_time += time
+        self.display_time += time
+        self.select_time += time
 
     # update the progressbar, reset when finished and add to score
     def update_bar(self, dt):
-        if self.currenttime < self.targettime - 1:
-            self.currenttime += 1
-            self.displaytime -= 0.01
+        if self.current_time < self.target_time - 1:
+            self.current_time += 1
+            self.display_time -= 0.05
         else:
             # stop the progress bar
             self.event.cancel()
             self.event = None
-            # reset targettime to correct value
-            self.targettime /= 100
+            # reset target_time to correct value
+            self.target_time /= 20
             # reset the opacity for the progress bar and start button
-            self.progressbaropacity = 0
-            self.startbuttonopacity = 100
-            self.currenttime = 0
+            self.progressbar_opacity = 0
+            self.start_button_opacity = 100
+            self.current_time = 0
+            self.display_time = self.select_time
             # add new value to score
-            self.app.score += self.targettime ** 1.2
+            self.app.score += self.target_time ** 1.2
             # enable buttons
             self.disabled = False
             # debugging info
-            print("Progress bar finished! Gained", round(self.targettime ** 1.2, 2),
+            print("Progress bar finished! Gained", round(self.target_time ** 1.2, 2),
                   "points. Score is now", round(self.app.score, 4))
 
     # start updating the progress bar, repeat the update function once every second
     def start_update(self):
         # check if value is valid
-        if self.targettime <= 0:
+        if self.target_time <= 0:
             pass
-        elif self.event is None and self.app.score >= self.targettime:
+        elif self.event is None and self.app.score >= self.target_time:
             # start the progress bar
-            self.event = Clock.schedule_interval(self.update_bar, 0.01)
-            self.startbuttonopacity = 0
-            self.progressbaropacity = 100
-            self.app.score -= self.targettime
+            self.event = Clock.schedule_interval(self.update_bar, 0.05)
+            self.start_button_opacity = 0
+            self.progressbar_opacity = 100
+            self.app.score -= self.target_time
             self.disabled = True
-            # multiply targettime to make progress bar smoother
-            self.targettime *= 100
+            # multiply target_time to make progress bar smoother
+            self.target_time *= 20
 
 
 class ProgressBarsApp(MDApp):
     # score variable
     score = NumericProperty(0)
-    newslotprice = NumericProperty(1000)
+    new_slot_price = NumericProperty(1000)
 
     # load the savefile to resume progress. autosave every second
     def __init__(self, **kwargs):
@@ -87,11 +91,11 @@ class ProgressBarsApp(MDApp):
             card.app = self
             self.cards.append(card)
             self.root.ids.box.add_widget(card)
-            self.score = self.score - self.newslotprice
+            self.score = self.score - self.new_slot_price
             # make next card more expensive
-            self.newslotprice = 1000 * len(self.cards) * 1.18
+            self.new_slot_price = 1000 * len(self.cards) * 0.95
 
-        if self.score > self.newslotprice:
+        if self.score > self.new_slot_price:
             add_card()
 
     def make_card_on_load(self):
@@ -105,7 +109,7 @@ class ProgressBarsApp(MDApp):
         with open("savefile.txt", mode="w") as f:
             savetext = str(self.score) + "\n"
             for card in self.cards:
-                savetext += str(card.currenttime) + "," + str(card.targettime) + "\n"
+                savetext += str(card.current_time) + "," + str(card.target_time) + "\n"
             f.write(savetext)
             f.close()
 
@@ -118,13 +122,21 @@ class ProgressBarsApp(MDApp):
             for line in f:
                 current, target = line.split(",")
                 self.make_card_on_load()
-                self.cards[-1].currenttime = current
-                self.cards[-1].targettime = target
-                self.cards[-1].displaytime = target
-                self.cards[-1].selecttime = target
+
                 if float(current) > 0:
-                    self.score += float((target.strip()))
+                    self.cards[-1].current_time = float(current)
+                    self.cards[-1].target_time = float(target) / 20
+                    self.cards[-1].display_time = float(current) / 20
+                    self.cards[-1].select_time = float(target) / 20
+
+                    self.score += float((target.strip())) / 20
                     self.cards[-1].start_update()
+                else:
+                    self.cards[-1].current_time = current
+                    self.cards[-1].target_time = target
+                    self.cards[-1].display_time = target
+                    self.cards[-1].select_time = target
+
             f.close()
 
         if os.path.exists("savefile.txt"):
